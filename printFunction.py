bde5bac37a3f63d359a75fd9cb0c270266573f12
Surendra419 <ysurendrareddy333@gmail.com>
2026-08-01 08:02:12 +0530

    Print function and Comments

---
+++ printFunction.py
@@ -0,0 +1,10 @@
+print("Hello Python!")
+print("-------------------------------------")
+print(2+ 3)
+print("-------------------------------------")
+print(" An escape sequence always starts with backslash \ followed by special character n \n this combination creates a newline ")
+print("-------------------------------------")
+# check above and bilow difference in escape characters
+print("-------------------------------------")
+print(" An escape sequence always starts with backslash \ followed by special character t \t this combination creates tab ")
+# print(2+3) this code as been commented now it won't excute
